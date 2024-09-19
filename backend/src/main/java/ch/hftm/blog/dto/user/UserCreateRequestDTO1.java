0c3cb819f0e04b57b2291039bbb5987a8fc74423
Hamsiga Rajaratnam <hamsiga.rajaratnam@ionesoft.ch>
2024-09-19 23:45:18 +0200

    Create User bug fixed

--- backend/src/main/java/ch/hftm/blog/dto/user/UserCreateRequestDTO1.java
+++ backend/src/main/java/ch/hftm/blog/dto/user/UserCreateRequestDTO1.java
@@ -4,6 +4,7 @@ import ch.hftm.blog.entity.User;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record UserCreateRequestDTO1(
         String firstName,
@@ -12,13 +13,17 @@ public record UserCreateRequestDTO1(
         @Email(message = "Email should be valid")
         String email,
         @NotNull(message = "Password must not be null nor blank nor empty")
-        String password) {
+        String password,
+        @NotBlank(message = "Role must not be null nor blank nor empty")
+        @Pattern(regexp = "^(user|admin)$", message = "Role must be either 'user' or 'admin'")
+        String role) {
     public User toUser() {
         User user = new User();
         user.setFirstName(firstName);
         user.setLastName(lastName);
         user.setEmail(email);
         user.setPassword(password);
+        user.setRole(role);
         return user;
     }
 }
